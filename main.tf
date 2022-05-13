4a1b80af83cde74253494215d4ee501c82475824
ccc-66 <masqu@163.com>
2022-05-13 14:49:12 +0800

    modify1 main.tf

--- main.tf
+++ main.tf
@@ -41,3 +41,16 @@ resource "aws_instance" "intest" {
   }
 
 }
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb1"
+  username             = "foo1"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
